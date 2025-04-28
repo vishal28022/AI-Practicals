32a2c76ca0780bc6bda45351d1e7ebe307070ab2
vishal28022 <158443302+vishal28022@users.noreply.github.com>
2025-04-28 09:48:42 +0530

    Update Pract_3.py

--- Pract_3.py
+++ Pract_3.py
@@ -1,111 +1,95 @@
-import sys 
+import sys
 
-class Graph():
-    def __init__(self, vertices):
-        self.v = vertices
-        self.graph = [[0 for column in range(vertices)]
-                      for row in range(vertices)]
-        self.edges = []
+def primMST(graph, vertices):
+    key = [sys.maxsize] * vertices
+    parent = [None] * vertices
+    key[0] = 0
+    mstSet = [False] * vertices
+    parent[0] = -1
 
-    def add_edges_from_adj_matrix(self):
-        for i in range(self.v):
-            for j in range(i, self.v):
-                if self.graph[i][j] != 0:
-                    self.edges.append((self.graph[i][j], i, j))
-        
-# ----------------- Prim's Algorithm -------------------
-    def print_prim_MST(self, parent):
-        print("Prim's MST:")
-        print("Edge \tWeight")
-        total_weight = 0
-        for i in range(1, self.v):
-            print(parent[i], "-", i, "\t", self.graph[parent[i]][i])
-            total_weight += self.graph[parent[i]][i]
-        print("Total weight of MST: ", total_weight)
+    for _ in range(vertices):
+        min_key = sys.maxsize
+        u = -1
+        for v in range(vertices):
+            if not mstSet[v] and key[v] < min_key:
+                min_key = key[v]
+                u = v
 
-    def minKey(self, key, mstSet):
-        min_val = sys.maxsize
-        min_index = -1
-        for v in range(self.v):
-            if key[v] < min_val and not mstSet[v]:
-                min_val = key[v]
-                min_index = v
-        return min_index
-    
-    def primMST(self):
-        key = [sys.maxsize] * self.v
-        parent = [None] * self.v
-        key[0] = 0 
-        mstSet = [False] * self.v
-        parent[0] = -1
+        mstSet[u] = True
 
-        for cout in range(self.v):
-            u = self.minKey(key, mstSet)
-            mstSet[u] = True
-            for v in range(self.v):
-                if self.graph[u][v] > 0 and mstSet[v] == False and key[v] > self.graph[u][v]:
-                    key[v] = self.graph[u][v]
-                    parent[v] = u
+        for v in range(vertices):
+            if graph[u][v] and not mstSet[v] and key[v] > graph[u][v]:
+                key[v] = graph[u][v]
+                parent[v] = u
 
-        self.print_prim_MST(parent)
+    print("\nPrim's MST:")
+    total_weight = 0
+    print("Edge \tWeight")
+    for i in range(1, vertices):
+        print(parent[i], "-", i, "\t", graph[parent[i]][i])
+        total_weight += graph[parent[i]][i]
+    print("Total weight of MST:", total_weight)
 
-# ------------------ Kruskal's Algorithm ---------------------
-    def find (self, parent, i):
-        if parent[i] != i:
-            parent[i] = self.find(parent, parent[i])
-        return parent[i]
+# ------------------------------------------------
+# ✅ Very Simple Version of Kruskal’s Algorithm:
 
-    def union(self, parent, rank, x, y):
-        xroot = self.find(parent, x)
-        yroot = self.find(parent, y)
+def find(parent, i):
+    if parent[i] != i:
+        parent[i] = find(parent, parent[i])
+    return parent[i]
 
-        if rank[xroot] < rank[yroot]:
-            parent[xroot] = yroot 
-        elif rank[xroot] > rank[yroot]:
-            parent[yroot] = xroot
-        else:
-            parent[yroot] = xroot 
-            rank[xroot] += 1
+def union(parent, rank, x, y):
+    xroot = find(parent, x)
+    yroot = find(parent, y)
+    if rank[xroot] < rank[yroot]:
+        parent[xroot] = yroot
+    elif rank[xroot] > rank[yroot]:
+        parent[yroot] = xroot
+    else:
+        parent[yroot] = xroot
+        rank[xroot] += 1
 
-    def kruskalMST(self):
-        self.edges.sort()
-        parent = [i for i in range(self.v)]
-        rank = [0] * self.v
+def kruskalMST(edges, vertices):
+    edges.sort()
+    parent = [i for i in range(vertices)]
+    rank = [0] * vertices
+    mst = []
+    total_weight = 0
 
-        mst = []
-        total_weight = 0
-        
-        for weight, u, v, in self.edges:
-            x = self.find(parent, u)
-            y = self.find(parent, v)
-            if x != y:
-                mst.append((u, v, weight))
-                self.union(parent, rank, x, y)
-                total_weight += weight
+    for weight, u, v in edges:
+        x = find(parent, u)
+        y = find(parent, v)
+        if x != y:
+            mst.append((u, v, weight))
+            union(parent, rank, x, y)
+            total_weight += weight
 
-        print("Kruskal's MST:")
-        print("Edge \t Weight")
-        for u, v, weight in mst:
-            print(u, "-", v, "\t", weight)
-        print("Total weight of MST:", total_weight)
+    print("\nKruskal's MST:")
+    print("Edge \tWeight")
+    for u, v, weight in mst:
+        print(u, "-", v, "\t", weight)
+    print("Total weight of MST:", total_weight)
 
-# --------------- Main function ---------------
-if __name__ == '__main__':
-    vertices = int(input("Enter the number of vertices: "))
-    g = Graph(vertices)
+# ------------------------------------------------
+# Main Program:
 
-    print("\nEnter the adjacency matrix (use 0 if no edge exists):")
-    for i in range(vertices):
-        row = list(map(int, input(f"Row {i + 1}: ").split()))
-        if len(row) != vertices:
-            print("Error: Please enter exactly", vertices, "numbers.")
-            sys.exit(1)
-        g.graph[i] = row
+vertices = int(input("Enter the number of vertices: "))
+graph = []
 
-    g.add_edges_from_adj_matrix()
+print("\nEnter the adjacency matrix (use 0 if no edge exists):")
+for i in range(vertices):
+    row = list(map(int, input(f"Row {i + 1}: ").split()))
+    if len(row) != vertices:
+        print("Error: Please enter exactly", vertices, "numbers.")
+        sys.exit(1)
+    graph.append(row)
 
-    print("\nRunning Prim's Algorithm...")
-    g.primMST()
+# Building the list of edges for Kruskal
+edges = []
+for i in range(vertices):
+    for j in range(i, vertices):
+        if graph[i][j] != 0:
+            edges.append((graph[i][j], i, j))
 
-    print("\nRunning Kruskal's Algorithm...")
-    g.kruskalMST()
+primMST(graph, vertices)
+kruskalMST(edges, vertices)
